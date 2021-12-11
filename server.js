d41bfce76e57876ad488560bed70c314a4c1d176
AniruddhaSoni <anirudh.soni007@gmail.com>
2021-12-11 19:43:30 +0530

    changed the port number

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 
-const port = 4000 || process.env.PORT
+const port = 8080 || process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server started on ${port} port`);
-})
+  console.log(`Server started on ${port} port`);
+});
