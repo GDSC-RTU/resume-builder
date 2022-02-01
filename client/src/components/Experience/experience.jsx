9c4c4cf835908cfa9d864f68da1f6617f39cb4bb
EightCubed <sushyam.s@gmail.com>
2022-02-02 00:57:11 +0530

    bug fixes

--- client/src/components/Experience/experience.jsx
+++ client/src/components/Experience/experience.jsx
@@ -32,16 +32,24 @@ class Experience extends React.Component {
       getYear(i){
         const d = new Date();
         let year = d.getFullYear();
-        let year_list=[];
-
-        for(let i=0;i<10;i++)
-          year_list.push(year+i);
-
-        return(year_list[i])
+      
+        return(year+i)
       }
 
       changeFunction(i){
         this.setState({start_month : i});
+      }
+
+      changeFunction1(i){
+        this.setState({start_year : i});
+      }
+
+      changeFunction2(i){
+        this.setState({end_month : i});
+      }
+
+      changeFunction3(i){
+        this.setState({end_year : i});
       }
 
       toggleChange=()=>{
@@ -113,19 +121,31 @@ class Experience extends React.Component {
                     {this.state.start_year}
               </Dropdown.Toggle>
               <Dropdown.Menu name="start_year" defaultValue={this.state.start_year}>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(0))}>{this.getYear(0)}</Dropdown.Item>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(1))}>{this.getYear(-1)}</Dropdown.Item>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(2))}>{this.getYear(-2)}</Dropdown.Item>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(3))}>{this.getYear(-3)}</Dropdown.Item>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(4))}>{this.getYear(-4)}</Dropdown.Item>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(5))}>{this.getYear(-5)}</Dropdown.Item>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(6))}>{this.getYear(-6)}</Dropdown.Item>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(7))}>{this.getYear(-7)}</Dropdown.Item>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(8))}>{this.getYear(-8)}</Dropdown.Item>
-                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction(this.getYear(9))}>{this.getYear(-9)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(0))}>{this.getYear(0)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-1))}>{this.getYear(-1)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-2))}>{this.getYear(-2)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-3))}>{this.getYear(-3)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-4))}>{this.getYear(-4)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-5))}>{this.getYear(-5)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-6))}>{this.getYear(-6)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-7))}>{this.getYear(-7)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-8))}>{this.getYear(-8)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-9))}>{this.getYear(-9)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-10))}>{this.getYear(-10)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-11))}>{this.getYear(-11)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-12))}>{this.getYear(-12)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-13))}>{this.getYear(-13)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-14))}>{this.getYear(-14)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-15))}>{this.getYear(-15)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-16))}>{this.getYear(-16)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-17))}>{this.getYear(-17)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-18))}>{this.getYear(-18)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-19))}>{this.getYear(-19)}</Dropdown.Item>
+                  <Dropdown.Item className="start_year" onClick={()=>this.changeFunction1(this.getYear(-20))}>{this.getYear(-20)}</Dropdown.Item>                  
                 </Dropdown.Menu>
               </Dropdown>
               </Col>
+              {!this.state.isChecked?<>
               <Col>
               End month
               <Dropdown>
@@ -133,18 +153,18 @@ class Experience extends React.Component {
                     {this.state.end_month}
                 </Dropdown.Toggle>
                 <Dropdown.Menu name="start_month" defaultValue={this.state.start_month}>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(0))}>{this.getMonth(0)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(1))}>{this.getMonth(1)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(2))}>{this.getMonth(2)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(3))}>{this.getMonth(3)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(4))}>{this.getMonth(4)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(5))}>{this.getMonth(5)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(6))}>{this.getMonth(6)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(7))}>{this.getMonth(7)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(8))}>{this.getMonth(8)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(9))}>{this.getMonth(9)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(10))}>{this.getMonth(10)}</Dropdown.Item>
-                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction(this.getMonth(11))}>{this.getMonth(11)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(0))}>{this.getMonth(0)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(1))}>{this.getMonth(1)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(2))}>{this.getMonth(2)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(3))}>{this.getMonth(3)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(4))}>{this.getMonth(4)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(5))}>{this.getMonth(5)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(6))}>{this.getMonth(6)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(7))}>{this.getMonth(7)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(8))}>{this.getMonth(8)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(9))}>{this.getMonth(9)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(10))}>{this.getMonth(10)}</Dropdown.Item>
+                  <Dropdown.Item className="end_month" onClick={()=>this.changeFunction2(this.getMonth(11))}>{this.getMonth(11)}</Dropdown.Item>
               </Dropdown.Menu>
               </Dropdown>
               </Col>
@@ -154,20 +174,31 @@ class Experience extends React.Component {
                 <Dropdown.Toggle id="dropdown-button-dark-example1" variant="dark">
                     {this.state.end_year}
                 </Dropdown.Toggle>    
-                <Dropdown.Menu name="end_year" defaultValue={this.state.start_year}>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(0))}>{this.getYear(0)}</Dropdown.Item>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(1))}>{this.getYear(1)}</Dropdown.Item>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(2))}>{this.getYear(2)}</Dropdown.Item>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(3))}>{this.getYear(3)}</Dropdown.Item>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(4))}>{this.getYear(4)}</Dropdown.Item>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(5))}>{this.getYear(5)}</Dropdown.Item>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(6))}>{this.getYear(6)}</Dropdown.Item>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(7))}>{this.getYear(7)}</Dropdown.Item>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(8))}>{this.getYear(8)}</Dropdown.Item>
-                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction(this.getYear(9))}>{this.getYear(9)}</Dropdown.Item>
+                <Dropdown.Menu name="end_year" defaultValue={this.state.end_year}>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(0))}>{this.getYear(0)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-1))}>{this.getYear(-1)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-2))}>{this.getYear(-2)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-3))}>{this.getYear(-3)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-4))}>{this.getYear(-4)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-5))}>{this.getYear(-5)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-6))}>{this.getYear(-6)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-7))}>{this.getYear(-7)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-8))}>{this.getYear(-8)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-9))}>{this.getYear(-9)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-10))}>{this.getYear(-10)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-11))}>{this.getYear(-11)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-12))}>{this.getYear(-12)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-13))}>{this.getYear(-13)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-14))}>{this.getYear(-14)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-15))}>{this.getYear(-15)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-16))}>{this.getYear(-16)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-17))}>{this.getYear(-17)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-18))}>{this.getYear(-18)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-19))}>{this.getYear(-19)}</Dropdown.Item>
+                  <Dropdown.Item className="end_year" onClick={()=>this.changeFunction3(this.getYear(-20))}>{this.getYear(-20)}</Dropdown.Item>                  
                 </Dropdown.Menu>
               </Dropdown>
-              </Col>
+              </Col></>:<></>}
             </Row>
               </Form.Group>
                   <div className="p-3">
