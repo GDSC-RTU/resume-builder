517f07f0b335c316f7659ba0b0b7d7aa6273e1a0
EightCubed <sushyam.s@gmail.com>
2022-02-13 02:53:17 +0530

    Added Linkedin API intergration using AWS Lambda

--- Server/Controllers/posts.js
+++
@@ -1,8 +0,0 @@
-export const getPosts = (req,res)=>{
-    console.log('backend is up!')
-    res.send('backend is up')
-}
-
-export const createPost = (req,res)=>{
-    console.log('creating post');
-}
